a258d241640e11a83e4c45c2c297d0bb5f5b87d8
Srikanth Kamalla <srikanthkamalla2919@gmail.com>
2025-05-12 21:36:21 +0530

    push 2

--- src/components/App.js
+++ src/components/App.js
@@ -1,4 +1,5 @@
 import React from "react";
+// import { Tooltip } from "react-tooltip";
 import "./../styles/App.css";
 import Tooltip from "./Tooltip";
 
@@ -6,17 +7,12 @@ const App = () => {
   return (
     <div>
       {/* Do not remove the main div */}
-      <div className="app-container">
-        <Tooltip text="This is a tooltip">
-          <h2 className="tooltip-heading">Hover over me</h2>
-        </Tooltip>
-        <br></br>
-        <Tooltip text="This is anothe tooltip">
-          <p className="tooltip-paragraph">
-            Hover over me to see another tooltip
-          </p>
-        </Tooltip>
-      </div>
+      <Tooltip text={<h2>This is a tooltip</h2>}>
+        {<h2>Hover over me</h2>}
+      </Tooltip>
+
+      <br></br>
+      <Tooltip text={<p>This is a tooltip</p>}>{<p>Hover over me</p>}</Tooltip>
     </div>
   );
 };
